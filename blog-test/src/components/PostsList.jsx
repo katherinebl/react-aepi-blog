20a4608cdb36e7daf30f400dea8c5ab7d1e330a8
Katherine <katherine.briceno@zooplus.com>
2021-03-01 23:12:39 +0100

    Request data and painted posts list

--- blog-test/src/components/PostsList.jsx
+++ blog-test/src/components/PostsList.jsx
@@ -1,8 +1,34 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
 function PostsList () {
+
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    axios.get('../../api/data.json')
+      .then(response => {
+        setPosts(response.data.posts)
+      })
+  }, [])
+
+
+  let postsList = []
+
+  postsList = posts.map(post => {
+    return (
+        <div className="post-card">
+          <h3>{post.title}</h3>
+          <p>{post.content}</p>
+          <small>{post.author}</small>
+        </div>
+    )
+  })
+
+
+
   return (
-    <div className="container">
-      <h2 className="posts-title">Posts List</h2>
-    </div>
+    <div className="container">{postsList}</div>
   )
 }
 
